21bc723e6fa8e666f22d61eea2f7f2e701c2fc5e
Thijs van Ede <t.s.vanede@utwente.nl>
2022-03-14 13:46:45 +0100

    Now explicitly use Mermaid template

--- diagrams/visualize/server.py
+++ diagrams/visualize/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template(
-        'tree.html',
+        'mermaid.html',
         classname = 'test',
         diagram   = diagram,
     )
